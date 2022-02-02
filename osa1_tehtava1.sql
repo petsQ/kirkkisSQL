652554012c4fb855abdc1ece8a1c703affe9abc2
petsQ <98839062+petsQ@users.noreply.github.com>
2022-02-02 10:55:49 +0200

    eka tehtava
    eka sql tehtava

---
+++ osa1_tehtava1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Henkilot(
+   henkilot_henkilo_id    INTEGER        PRIMARY KEY,
+   henkilot_etunimi       VARCHAR(40)   NOT NULL,
+   henkilot_sukunimi      VARCHAR(40)   NOT NULL,
+   henkilot_email         VARCHAR(100)
+)
